f8f56e9c168726d28235ab98b4cb33457b599c92
giedriuspalinauskas <zepirex13@gmail.com>
2020-01-14 18:08:22 +0200

    Projektas
    Darau projekta Kaunas coding school

---
+++ e-shopas/admin-newpreke.php
@@ -0,0 +1,93 @@
+<?php
+ include('header.php') ;
+?>
+<div class="contrainer border m-2">
+  <a class="btn bg-dark text-white m-2" href="admin-prekes.php"> < Atgal </a>
+  <div class="row">
+    <div class="col d-flex justify-content-center">
+      <h1>Prideti preke</h1>
+    </div>
+  </div>
+  <form class="" action="controler/createPreke.php" method="post">
+  <div class="row p-2 mt-1 ">
+    <div class="col-2">
+      <label for="">Pavadinimas: </label>
+    </div>
+    <div class="col">
+      <input style="width: 100%; height:30px" type="text" name="pavadinimas" value="">
+    </div>
+  </div>
+  <div class="row p-2 mt-1">
+    <div class="col-2">
+      <label for="">Antraste : </label>
+    </div>
+    <div class="col">
+      <textarea style="width: 100%" name="antraste" rows="8" cols="80"></textarea>
+    </div>
+  </div>
+  <div class="row p-2 mt-1 ">
+    <div class="col-2">
+      <label for="">Nuolaida : </label>
+    </div>
+    <div class="col">
+      <input style="width: 100%; height:30px" type="text" name="nuolaida" value="">
+    </div>
+  </div>
+  <div class="row p-2 mt-1 ">
+    <div class="col-2">
+      <label for="">Kiekis : </label>
+    </div>
+    <div class="col">
+      <input style="width: 100%; height:30px" type="text" name="kiekis" value="">
+    </div>
+  </div>
+  <div class="row p-2 mt-1 ">
+    <div class="col-2">
+      <label for="">Pozicija : </label>
+    </div>
+    <div class="col">
+      <select name="pozicija" value="" class="form-control">
+        <option></option>
+        <option>rodyti</option>
+        <option>nerodyti</option>
+      </select>
+    </div>
+  </div>
+  <div class="row p-2 mt-1 ">
+    <div class="col-2">
+      <label for="">Kategorija : </label>
+    </div>
+    <div class="col">
+      <select name="kategorija" value="" class="form-control">
+        <option></option>
+        <option>raktu</option>
+        <option>kaklo</option>
+        <option>automobiliu</option>
+      </select>
+    </div>
+  </div>
+  <div class="row p-2 mt-1 ">
+    <div class="col-2">
+      <label for="">Lytis : </label>
+    </div>
+    <div class="col">
+      <select name="lytis" value="lytis" class="form-control">
+        <option></option>
+        <option>vyr</option>
+        <option>mot</option>
+      </select>
+    </div>
+  </div>
+  <div class="row p-2 mt-1 ">
+    <div class="col d-flex justify-content-end">
+      <button class="btn bg-dark text-white" type="submit" name="button">Prideti preke</button>
+    </div>
+  </div>
+  </form>
+  <!-- containerio pabaiga -->
+</div>
+
+
+
+
+<?php include('footer.php');?>
---
+++ e-shopas/js/main.js
@@ -0,0 +1,165 @@
+console.log("labas");
+
+// TIKRINIMAS registracijos ar visi laukai tinka ir
+ // jeigu tinka naudojamas ajax ir viska ideda i DB
+$('#Registracija').on("click", function(e) {
+  let vardas =         $('#firstName').val();
+  let pavarde =        $('#lastName').val();
+  let slaptazodis =    $('#password-prisijungimas').val();
+  let pslaptazodis =   $('#confirm_password').val();
+  let mail =           $('#email').val();
+  let adresas =        $('#adress').val();
+  let miestas =        $('#city').val();
+  let pkodas =         $('#zip').val();
+  let salis =          $('#county').val();
+  let telnum =         $('#tel').val();
+  let tikrinimas = "False";
+mail = mail.toLowerCase();
+
+// let trinimas = $('#pranesimas').empty();
+
+if (vardas === "") {
+  $('#firstName').css({"border-color":"red"});
+  $('#msg_vardas').html("<li class='bg-warning mt-1'>* Iveskite varda !</li>");
+  tikrinimas = "True";
+} else {
+            $('#firstName').css({"border-color":"green"});
+            $('#msg_vardas').empty();
+       }
+
+if (pavarde === "") {
+  $('#lastName').css({"border-color":"red"});
+  $('#msg_lastName').html("<li class='bg-warning mt-1'>* Iveskite pavarde !</li>");
+  tikrinimas = "True";
+} else {
+            $('#lastName').css({"border-color":"green"});
+            $('#msg_lastName').empty();
+       }
+if (adresas === "") {
+  $('#adress').css({"border-color":"red"});
+  $('#msg_adress').html("<li class='bg-warning mt-1'>* Iveskite adresa !</li>");
+  tikrinimas = "True";
+} else {
+            $('#adress').css({"border-color":"green"});
+            $('#msg_adress').empty();
+       }
+
+if (miestas === "") {
+  $('#city').css({"border-color":"red"});
+  $('#msg_city').html("<li class='bg-warning mt-1'>* Iveskite miesta !</li>");
+  tikrinimas = "True";
+} else {
+            $('#city').css({"border-color":"green"});
+            $('#msg_city').empty();
+       }
+
+if (pkodas === "") {
+  $('#zip').css({"border-color":"red"});
+  $('#msg_zip').html("<li class='bg-warning mt-1'>* Iveskite pasto koda !</li>");
+  tikrinimas = "True";
+} else {
+            $('#zip').css({"border-color":"green"});
+            $('#msg_zip').empty();
+       }
+
+if (salis === "") {
+  $('#county').css({"border-color":"red"});
+  $('#msg_county').html("<li class='bg-warning mt-1'>* Iveskite varda !</li>");
+  tikrinimas = "True";
+} else {
+            $('#county').css({"border-color":"green"});
+            $('#msg_county').empty();
+       }
+
+if (telnum === "") {
+  $('#tel').css({"border-color":"red"});
+  $('#msg_tel').html("<li class='bg-warning mt-1'>* Iveskite telefono numeri !</li>");
+  tikrinimas = "True";
+} else {
+            $('#tel').css({"border-color":"green"});
+            $('#msg_tel').empty();
+       }
+
+if (slaptazodis === "" ) {
+  $('#password-prisijungimas').css({"border-color":"red"});
+  $('#msg_password').append("<li class='bg-warning mt-1'>* Iveskite slaptazodi !</li>");
+  tikrinimas = "True";
+} else if (slaptazodis.length < 8) {
+  $('#password-prisijungimas').css({"border-color":"red"});
+  $('#msg_password').append("<li class='bg-warning mt-1'>* Slaptazodis turi buti ilgesnis nei 8 simboliai !</li>");
+    tikrinimas = "True";
+} else if (slaptazodis !== pslaptazodis) {
+  $('#confirm_password').css({"border-color":"red"});
+  $('#password-prisijungimas').css({"border-color":"red"});
+  $('#msg_confirm_password').html("<li class='bg-warning mt-1'>* Slaptazodziai nesutampa !</li>");
+  tikrinimas = "True";
+} else { $('#confirm_password').css({"border-color":"green"});
+         $('#password-prisijungimas').css({"border-color":"green"});
+         $('#confirm_password').empty();
+         $('#msg_confirm_password').empty();
+        }
+
+        function IsEmail(email) {
+          var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+          if(!regex.test(email)) {
+             return false;
+          }else{
+             return true;
+          }
+        }
+
+        if(IsEmail(mail)==false){
+          $('#email').css({"border-color":"red"});
+          $('#msg_email').html("<li class='bg-warning mt-1'>* Iveskite El.Pasta !</li>");
+        } else {
+          $('#email').css({"border-color":"green"});
+          $('#msg_email').empty();
+        }
+
+  if (tikrinimas === "True") {
+    e.preventDefault();
+  } else {
+    $.ajax({
+      url: "controler/createVartotojas.php",
+      type: "POST",
+      data: {
+            vardas: vardas,
+            pavarde: pavarde,
+            slaptazodis: slaptazodis,
+            mail: mail,
+            adresas: adresas,
+            miestas: miestas,
+            pkodas: pkodas,
+            salis: salis,
+            telNumeris: telnum
+          },
+    success: function() {
+      alert("Prisiregistravote !");
+    }
+  });
+}
+}); //REGISTRACIJOS FUNKCIJOS PABAIGA
+
+//prisijungimas
+//Tikrina ar visi laukai yra geri
+//viska nusuncia i Db naudojant ajax
+$('#Prisijungti').on('click', function() {
+  let email = $('#prisijungimas').val();
+  let password = $('#password').val();
+
+  $.ajax({
+  type: "POST",
+  url: "controler/prisijungimas.php",
+  data:{email: email, password: password},
+  success: function(massage) {
+      if (massage === "True") {
+        alert('Pavyko prisijungti !');
+        window.location.replace("index.php");
+      } else if (massage === "False") {
+        alert('Netiko El.pastas arba slaptazodis');
+        location.reload();
+      }
+  }
+});
+
+}); //Prisijungimo funkcijos pabaiga
