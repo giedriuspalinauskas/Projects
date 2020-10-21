bbf4d6b4466023894a2469fab40bff978fd9bfed
Giedrius <58683099+giedriuspalinauskas@users.noreply.github.com>
2020-10-21 21:53:33 +0300

    Darbu pradzia

--- To-do(riawolf)/index.php
+++ To-do(riawolf)/index.php
@@ -4,26 +4,41 @@
         <meta charset="utf-8">
         <title></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <link rel="stylesheet" href="css/normalize.css">
-
         <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
-
-        <!-- MANO CSS visada pats zemiausias!!! -->
         <link rel="stylesheet" href="css/style.css">
     </head>
+
     <body>
-        <div class="container  bg-light">
-             <h1>Isijuk inspect->console</h1>
-
+      <!-- -------------------------------FORMA DARBO UZDUOCIAI ------------------------------------- -->
+      <div class="text-center mb-5">
+        <h1>TO-DO</h1>
+      </div>
+      <form class="container">
+      <div class="form-group">
+        <label for="pavadinimas">Pavadinimas</label>
+        <input type="text" class="form-control mb-2" id="pavadinimas" name="pavadinimas">
+        <p id="msg_pavadinimas"></p>
+        <label>Jūsų darbo užduotis</label>
+        <!-- textarea duomenu bazeja telpa tik 500 simboliu -->
+        <textarea class="form-control" rows="5" id="tekstas" name="antraste" maxlength="500"></textarea>
+        <p id="msg_antraste"></p>
+        <div class="float-right">
+          <button class="btn btn-primary mt-3" id="forma">Įrašyti</button>
         </div>
+      </div>
+    </form>
 
-
-
+    <?php
+      $tekstai = getTekstai();
+      // print_r($tekstai);
+      // while ($tekstas = mysqli_fetch_assoc($tekstai)){
+      //   print_r($tekstas);
+      // }
+     ?>
 
         <script type="text/javascript" src="libs/jquery-3.4.1.min.js"> </script>
         <script type="text/javascript" src="libs/bootstrap/js/bootstrap.bundle.min.js"> </script>
-        <!-- MANO JS pats zemiausias -->
         <script type="text/javascript" src="js/main.js"></script>
 
      </body>
--- To-do(riawolf)/js/main.js
+++ To-do(riawolf)/js/main.js
@@ -1,4 +1,46 @@
 // test
 console.log("labas");
 // alert("issokanti zinute");
-document.write("labas ekrane");
+// document.write("labas ekrane");
+
+
+$('#forma').on("click", function(error) {
+  let pavadinimas = $('#pavadinimas').val();
+  let tekstas = $('#tekstas').val();
+  let tikrinimas = "false";
+
+  console.log(tekstas,pavadinimas);
+  if (pavadinimas === "") {
+    $('#pavadinimas').css({"border-color":"red"});
+    $('#msg_pavadinimas').html("<p class='bg-warning mt-1'>* Iveskite pavadinima !</p>");
+    tikrinimas = "True";
+  } else {
+              $('#pavadinimas').css({"border-color":"green"});
+              $('#msg_pavadinimas').empty();
+         }
+  if (tekstas === "") {
+    $('#tekstas').css({"border-color":"red"});
+    $('#msg_antraste').html("<p class='bg-warning mt-1'>* Iveskite darbo uzduoti !</p>");
+    tikrinimas = "True";
+  } else {
+              $('#tekstas').css({"border-color":"green"});
+              $('#msg_antraste').empty();
+         }
+
+  if (tikrinimas === "True") {
+     error.preventDefault();
+   } else {
+     $.ajax({
+       url: "controler/createTeksta.php",
+       type: "POST",
+       data: {
+             pavadinimas: pavadinimas,
+             antraste: tekstas
+           },
+     success: function() {
+       alert("pavyko!")
+     }
+   });
+ }
+
+});
